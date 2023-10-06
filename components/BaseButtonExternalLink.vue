24070cd69c7d4fae88496f2792378ed58c0aaaf4
Oldrich <oldrich@lqd.cz>
2023-10-06 13:49:34 +0200

    linter

--- components/BaseButtonExternalLink.vue
+++ components/BaseButtonExternalLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <a class="btn btn-outline-alternate btn-sm me-1"><i class="fa fa-external-link" /></a>
+  <a class="btn btn-outline-alternate btn-sm me-1"><i class="fa fa-external-link px-1" /></a>
 </template>
 
 <script setup lang="ts">
--- pages/delivery-types/new.vue
+++ pages/delivery-types/new.vue
@@ -16,57 +16,63 @@
     <template #body>
       <div class="row">
       <BaseForm ref="form" wrap="col-lg-6" url="customer" :lang="lang" :input="input" :rules="rules" @success="success">
+        <h5 class="card-title">Nová doprava</h5>
         <div class="row">
           <BaseTextBoxLocale name="name" wrap="col-lg-8" label="Název" type="text" />
           <div class="col-lg-4">
             <label>Kód</label>
             <div class="input-group">
-              <input type="text" class="form-control form-control-sm">
-              <div class="input-group-append">
-                <button class="btn btn-sm btn-outline-primary"><i class="fa fa-refresh"></i></button>
-              </div>
+              <BaseTextBox name="code" />
+              <BaseButton wrap="input-group-append" type="button" class="btn-sm btn-outline-primary"><i class="fa fa-refresh"></i></BaseButton>
             </div>
           </div>
         </div>
         <div class="row mt-2">
-          <BaseTextBox name="ic" wrap="col-lg-10" label="Odkaz pro sledování zásilky" type="text" />
-          <BaseTextBox name="company" wrap="col-lg-2" label="Priorita" type="text" />
+          <BaseTextBox name="trackingLink" wrap="col-lg-10" label="Odkaz pro sledování zásilky" type="text" />
+          <BaseTextBox name="priority" wrap="col-lg-2" label="Priorita" type="text" />
         </div>
         <div class="row mt-2">
-          <BaseTextBox name="ic" wrap="col-lg-6" label="Výchozí zobrazované doručení" type="text" />
-          <BaseTextBox name="company" wrap="col-lg-6" label="Exkluzivní pro skupinu uživatelů" type="text" />
+          <BaseTextBox name="defaultDisplayDelivery" wrap="col-lg-6" label="Výchozí zobrazované doručení" type="text" />
+          <BaseTextBox name="exclusive" wrap="col-lg-6" label="Exkluzivní pro skupinu uživatelů" type="text" />
         </div>
         <div class="row mt-2">
-          <BaseTextBox name="ic" wrap="col-lg-6" label="Typ výdejních míst" type="text" />
-          <BaseTextBox name="company" wrap="col-lg-6" label="Povolené typy plateb" type="text" />
+          <BaseTextBox name="pickupPointType" wrap="col-lg-6" label="Typ výdejních míst" type="text" />
+          <BaseTextBox name="allowedPaymentTypes" wrap="col-lg-6" label="Povolené typy plateb" type="text" />
+        </div>
+        <div class="row mt-3">
+          <div class="d-flex gap-3">
+            <BaseCheckBox name="recommended" label="Doporučení" wrap="flex-shrink-0" />
+            <BaseCheckBox name="hidden" label="Skryto" wrap="flex-shrink-0" />
+            <BaseCheckBox name="externalCarrier" label="Externí dopravce" wrap="flex-shrink-0" />
+          </div>
         </div>
         <div class="form-wrapper-blue mt-3">
             <h5 class="card-title">Maximalní rozměry a váha na přepravu</h5>
           <div class="row">
-            <BaseTextBox name="ic" wrap="col-lg-4" label="IC" type="text" />
-            <BaseTextBox name="company" wrap="col-lg-4" label="Company" type="text" />
-            <BaseTextBox name="company" wrap="col-lg-4" label="Company" type="text" />
+            <BaseTextBox name="maxWidth" wrap="col-lg-4" label="Šírka" type="text" />
+            <BaseTextBox name="maxLength" wrap="col-lg-4" label="Délka" type="text" />
+            <BaseTextBox name="maxDepth" wrap="col-lg-4" label="Hloubka" type="text" />
           </div>
             <div class="row mt-2">
-              <BaseTextBox name="ic" wrap="col-lg-4" label="IC" type="text" />
+              <BaseTextBox name="maxWeight" wrap="col-lg-4" label="Váha" type="text" />
             </div>
         </div>
         <div class="row mt-2">
-          <BaseTextAreaLocale name="ic" wrap="col-lg-12" label="Perex" type="text" rows="4" />
+          <BaseTextAreaLocale name="perex" wrap="col-lg-12" label="Perex" type="text" rows="4" />
         </div>
         <div class="row mt-2">
-          <BaseTextAreaLocale name="ic" wrap="col-lg-12" label="Instrukce (např. do emailu)" type="text" rows="4" />
+          <BaseTextAreaLocale name="text" wrap="col-lg-12" label="Instrukce (např. do emailu)" type="text" rows="4" />
         </div>
         <div class="form-wrapper-blue mt-3">
 
 
           <h5 class="card-title mt-2">Externí ID</h5>
         <div class="row mt-2">
-          <BaseTextBox name="ic" wrap="col-lg-6" label="Obecné" type="text" />
-          <BaseTextBox name="company" wrap="col-lg-6" label="Heuréka.cz" type="text" />
+          <BaseTextBox name="externalId" wrap="col-lg-6" label="Obecné" type="text" />
+          <BaseTextBox name="externalIdHeureka" wrap="col-lg-6" label="Heuréka.cz" type="text" />
         </div>
         <div class="row mt-2">
-          <BaseTextBox name="ic" wrap="col-lg-6" label="Zboží.cz" type="text" />
+          <BaseTextBox name="externalIdZbozi" wrap="col-lg-6" label="Zboží.cz" type="text" />
           <BaseTextBox name="company" wrap="col-lg-6" label="Pohoda" type="text" />
         </div>
           <div class="row mt-3">
@@ -86,30 +92,30 @@
         <div class="form-wrapper-light mt-3">
 
           <div class="row">
-            <BaseTextBox name="phone" wrap="col-lg-4" label="Cena" type="text" />
-            <BaseTextBox name="email" wrap="col-lg-4" label="Cena s DPH" type="text" />
-            <BaseTextBox name="email" wrap="col-lg-2" label="Měna" type="text" />
+            <BaseTextBox v-model="input.price" name="price" wrap="col-lg-3" label="Cena" type="number" @change="input.priceVat = Math.round(parseInt(input.price) * 1.21)" />
+            <BaseTextBox v-model="input.priceVat" wrap="col-lg-3" label="Cena s DPH" type="number" @change="input.price = Math.round(parseInt(input.priceVat) / 1.21)" />
+            <BaseMultiSelect wrap="col-lg-3" label="Měna" v-model="input.currency" options-url="currency" :options-url-params="{method: 'POST', body: {'_op': 'optionsList'}}" />
           </div>
           <div class="row mt-2">
-            <BaseTextBox name="phone" wrap="col-lg-4" label="Dostupné do váhy" type="text" />
-            <BaseTextBox name="email" wrap="col-lg-4" label="Dostupné do rozměru" type="text" />
+            <BaseTextBox name="phone" wrap="col-lg-3" label="Dostupné do váhy" type="text" />
+            <BaseTextBox name="email" wrap="col-lg-3" label="Dostupné do rozměru" type="text" />
             <div class="col-lg-4">
               <label>&nbsp;</label><br>
-              <button class="btn btn-sm btn-outline-secondary me-2"><i class="fa fa-trash-o" /></button>
+              <button class="btn btn-sm btn-outline-danger me-2"><i class="fa fa-trash-o" /></button>
             </div>
           </div>
           <hr class="pt-1" style="color: white">
           <div class="row">
-            <BaseTextBox name="phone" wrap="col-lg-4" label="Cena" type="text" />
-            <BaseTextBox name="email" wrap="col-lg-4" label="Cena s DPH" type="text" />
-            <BaseTextBox name="email" wrap="col-lg-2" label="Měna" type="text" />
+            <BaseTextBox v-model="input.price" name="price" wrap="col-lg-3" label="Cena" type="number" @change="input.priceVat = Math.round(parseInt(input.price) * 1.21)" />
+            <BaseTextBox v-model="input.priceVat" wrap="col-lg-3" label="Cena s DPH" type="number" @change="input.price = Math.round(parseInt(input.priceVat) / 1.21)" />
+            <BaseMultiSelect wrap="col-lg-3" label="Měna" v-model="input.currency" options-url="currency" :options-url-params="{method: 'POST', body: {'_op': 'optionsList'}}" />
           </div>
           <div class="row mt-2">
-            <BaseTextBox name="phone" wrap="col-lg-4" label="Dostupné do váhy" type="text" />
-            <BaseTextBox name="email" wrap="col-lg-4" label="Dostupné do rozměru" type="text" />
+            <BaseTextBox name="phone" wrap="col-lg-3" label="Dostupné do váhy" type="text" />
+            <BaseTextBox name="email" wrap="col-lg-3" label="Dostupné do rozměru" type="text" />
             <div class="col-lg-4">
               <label>&nbsp;</label><br>
-              <button class="btn btn-sm btn-outline-secondary me-2"><i class="fa fa-trash-o" /></button>
+              <button class="btn btn-sm btn-outline-danger me-2"><i class="fa fa-trash-o" /></button>
             </div>
           </div>
           <div class="mt-3">
@@ -127,7 +133,6 @@
 import { inject } from 'vue';
 import {ToastPluginApi, useToast} from 'vue-toast-notification';
 import {required} from "@vuelidate/validators";
-import Editor from '@tinymce/tinymce-vue';
 
 const toast: ToastPluginApi = inject('toast', useToast());
 
@@ -138,8 +143,9 @@ const langs = ref(['cs', 'en']);
 const input:any = ref({ });
 
 const rules = {
-  login: { required },
-  password: { required },
+  name: { cs: {required} , en: {required} },
+  priority: { required },
+  code: { required },
 };
 
 
