432c3d7978bed37b5f077357a7393a2d9b6bd778
MariMens <148960213+MariMens@users.noreply.github.com>
2025-01-20 16:35:01 +0100

    card, table, filter

---
+++ components/amount/TableFilter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+withDefaults(defineProps<{
+  modelValue?: any | null,
+}>(), { modelValue: undefined });
+
+const emit = defineEmits(['update:modelValue']);
+
+</script>
+
+<template>
+  <BaseFilter @cancel="emit('update:modelValue', {})">
+    <BaseTextBox v-model="modelValue['f-label%']" wrap="flex-shrink-0" placeholder="Popisek" type="text" />
+    <BaseButton class="btn btn-sm btn-primary">Filtrovat</BaseButton>
+  </BaseFilter>
+</template>
---
+++ pages/amount/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { filters, lang } = useTableVars();
+</script>
+
+<template>
+  <BasePageHeader :title="'Dostupnost'" :icon="'icon'" />
+  <BaseCard wrap="col-lg-12">
+    <template #headerLeft>
+      <AmountTableFilter v-model="filters" />
+    </template>
+    <template #headerRight>
+      <BaseButtonNew class="btn-sm" @click="navigateTo({ name: 'amount-new'})">Nová položka</BaseButtonNew>
+    </template>
+    <template #body>
+      <AmountTable :filters="filters" :lang="lang" />
+    </template>
+  </BaseCard>
+</template>
