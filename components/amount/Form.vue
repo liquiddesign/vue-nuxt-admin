f2595cd6929c486ef63264874444ac713f8dd140
MariMens <148960213+MariMens@users.noreply.github.com>
2025-01-21 13:40:10 +0100

    forms

--- components/amount/Form.vue
+++ components/amount/Form.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import {RouteParamValue} from 'vue-router';
+import {required} from '@vuelidate/validators';
 
 withDefaults(defineProps<{
   url: string,
@@ -8,13 +9,42 @@ withDefaults(defineProps<{
   loading?: boolean,
   slug?: string|RouteParamValue[],
 }>(), { loading: false, slug: undefined });
+
+const form = ref(null);
+const goBack: Ref<boolean> = ref(false);
+
+const rules = {
+  priority: {required},
+  label: {
+    cs: {required},
+  },
+};
+
+function submit()
+{
+  form.value?.submit();
+}
+
+
+const $emit = defineEmits(['success']);
+defineExpose({ submit } );
+
 </script>
 
 <template>
-  <BaseForm ref="form" :url="url" :lang="lang" :data="data" :slug="slug" :loading="loading">
+  <BaseForm ref="form" :url="url" :lang="lang" :data="data" :slug="slug" :rules="rules" :loading="loading" @success="$emit('success', goBack ? null : $event.result); goBack = false">
     <h5 class="card-title">HLAVNÍ ÚDAJE</h5>
-    <BaseTextBox :wrap="'col-3 row flex-nowrap'" :label="'Popisek'" :type="'text'" locale />
-    <BaseTextBox :wrap="'col-3 row flex-nowrap mt-2'" :label="'Priorita'" :type="'number'" required />
-    <BaseMultiSelect :wrap="'col-3 row flex-nowrap mt-2'" :label="'Přednastavené doručení'" :type="'text'" options-url="eshop/display-delivery?property=label" :options-url-params="{method: 'POST', body: {'_op': 'list'}}" />
+    <BaseTextBoxLocale wrap="col-3 row flex-nowrap" name="label" label="Popisek" type="text" />
+    <BaseTextBox wrap="col-3 row flex-nowrap mt-2" name="priority" label="Priorita" type="number" required />
+    <BaseMultiSelect wrap="col-3 row flex-nowrap mt-2" name="displayDelivery" label="Přednastavené doručení" type="text" options-url="eshop/display-delivery?property=label" :options-url-params="{method: 'POST', body: {'_op': 'list'}}" />
+    <i class="fa fa-info-circle" /> Pokud nastavíte "Přednastavené doručení", tak u productů s nastaveným doručením na "Automaticky" bude zvoleno toto doručení.
+    <BaseCheckBox name="isSold" wrap="col-3 row flex-nowrap mt-2"> Označit jako vyprodáno </BaseCheckBox>
+
+    <div class="row mt-3">
+      <div class="col-lg-6">
+        <BaseFormButton class="btn-success btn-sm me-1" @click="goBack = true;">Uložit</BaseFormButton>
+        <BaseFormButton class="btn-outline-primary me-1 btn-sm" @click="goBack = false;">Uložit a pokračovat</BaseFormButton>
+      </div>
+    </div>
   </BaseForm>
 </template>
--- pages/amount/new.vue
+++ pages/amount/new.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
 
+const lang = ref('cs');
+const langs = ref(['cs', 'en']);
+
+const formData:any = ref({ label: {cs: '', en: ''}, priority: 0, displayDelivery: null, isSold: false});
+
+function redirect(id) {
+  formData.value = { label: {cs: '', en: ''}, priority: 0, displayDelivery: null, isSold: false};
+  id ? navigateTo({name: 'amount-id', params: {id: id}}) :  navigateTo({name: 'amount'});
+}
+
 </script>
 
 <template>
-  <div>123</div>
+  <BaseCard>
+    <template #headerLeft>
+      <BaseButtonBack class="me-2" @click="navigateTo({name: 'amount'})" />
+      <BaseButtonSave class="btn-sm me-2" @click="$refs?.form.submit()" />
+      <BaseLanguageList :langs="langs" :lang="lang" @select="lang = $event" />
+    </template>
+    <template #body>
+      <AmountForm ref="form" :lang="lang" :data="formData" @success="redirect" />
+    </template>
+  </BaseCard>
 </template>
