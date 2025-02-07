087dc3e79bc2506be0b1cb0b52aaba83d8ebba7b
MariMens <148960213+MariMens@users.noreply.github.com>
2025-02-07 14:03:36 +0100

    price table

---
+++ components/ThePriceTable.vue
@@ -0,0 +1,50 @@
+<template>
+  <BaseCard wrap="col-lg-12">
+<!--    <template #header>-->
+
+<!--    </template>-->
+    <BaseGrid ref="grid" :url="url" :page="page" :on-page="onPage" :filters="filters" :order="order" @change-order="emit('update:order', $event)">
+      <!--    <template #header>-->
+      <!--      <tr>-->
+      <!--        <BaseGridTh class="minimal" order-by="id">#</BaseGridTh>-->
+      <!--        <BaseGridTh class="minimal" order-by="product">Kód</BaseGridTh>-->
+      <!--        <BaseGridTh order-by="product">Název</BaseGridTh>-->
+      <!--        <BaseGridTh class="minimal" order-by="price">Cena</BaseGridTh>-->
+      <!--        <BaseGridTh class="minimal" order-by="priceVat">Cena s DPH</BaseGridTh>-->
+      <!--        <BaseGridTh class="minimal" order-by="priceVat">Cena před slevou</BaseGridTh>-->
+      <!--        <BaseGridTh class="minimal" order-by="priceVat">Cena s DPH před slevou</BaseGridTh>-->
+      <!--        <BaseGridTh class="minimal" order-by="hidden"><i class="far fa-eye-slash" /></BaseGridTh>-->
+      <!--      </tr>-->
+      <!--    </template>-->
+      <template #body="{item, updateRow}">
+        <tr>
+          <td class="minimal">{{ item.id }}  ☰</td>
+          <td class="minimal">{{ item.product.code ?? '-' }}</td>
+          <td>{{ item.product.name ?? '-' }}</td>
+          <td class="minimal"><BaseTextBox v-model="item.price" type="number" class="form-control-xs" style="width: 50px;" @change="(e) => updateRow(parseInt(e.target.value), 'price')" /></td>
+          <td class="minimal"><BaseTextBox v-model="item.priceVat" type="number" class="form-control-xs" style="width: 50px;" @change="(e) => updateRow(parseInt(e.target.value), 'priceVat')" /></td>
+          <td class="minimal"><BaseTextBox v-model="item.priceBefore" type="number" class="form-control-xs" style="width: 50px;" @change="(e) => updateRow(parseInt(e.target.value), 'priceBefore')" /></td>
+          <td class="minimal"><BaseTextBox v-model="item.priceVatBefore" type="number" class="form-control-xs" style="width: 50px;" @change="(e) => updateRow(parseInt(e.target.value), 'priceVatBefore')" /></td>
+          <td class="minimal"><BaseCheckBox v-model="item.hidden" @change="(e) => updateRow(e.target.checked, 'hidden')" /></td>
+        </tr>
+      </template>
+      <template #footer>
+        <BaseGridPaginator wrap="flex-shrink-0" :url="url" :page="page" :on-page="onPage" :filters="filters" @change-page="emit('update:page', $event)" @change-on-page="emit('update:onPage', $event); page = 1;" />
+      </template>
+    </BaseGrid>
+  </BaseCard>
+</template>
+
+<script setup lang="ts">
+import type {GridOrder} from '~/composables/useTableVars';
+
+withDefaults(defineProps<{
+  filters?: any,
+  page: number,
+  onPage: number,
+  order: GridOrder,
+}>(), { filters: {}  });
+
+const url: string = 'eshop/price?expand=product';
+const emit = defineEmits(['update:page', 'update:onPage', 'update:order']);
+</script>
--- components/pricelist/Form.vue
+++ components/pricelist/Form.vue
@@ -1,49 +1,57 @@
 <template>
   <BaseForm ref="form" :url="url" :data="data" :slug="slug" :rules="rules" :loading="loading" @success="$emit('success', goBack ? null : $event.result); goBack = false">
-    <h5 class="card-title">HLAVNÍ ÚDAJE</h5>
-    <div class="col-lg-6">
-      <div class="row">
-        <BaseTextBox wrap="col-lg-8" name="name" label="Název" type="text" required />
-        <BaseTextBox wrap="col-lg-4" name="code" label="Kód" type="text" required />
-      </div>
-
-      <div class="row mt-2">
-        <BaseTextArea wrap="col-lg-12" name="description" label="Popis" />
-      </div>
-
-      <div class="form-wrapper-light mt-3">
+    <div class="row">
+      <div class="col-lg-4">
+        <h5 class="card-title">HLAVNÍ ÚDAJE</h5>
         <div class="row">
-          <BaseMultiSelect wrap="col-lg-4" name="currency" label="Měna" options-url="eshop/currency?property=code" :options-url-params="{method: 'POST', body: {'_op': 'list'}}" />
-          <BaseMultiSelect wrap="col-lg-8" name="country" label="Země DPH" options-url="eshop/country?property=name" :options-url-params="{method: 'POST', body: {'_op': 'list'}}" />
+          <BaseTextBox wrap="col-lg-8" name="name" label="Název" type="text" required />
+          <BaseTextBox wrap="col-lg-4" name="code" label="Kód" type="text" required />
         </div>
 
         <div class="row mt-2">
-          <BaseMultiSelect wrap="col-lg-8" name="discounts" label="Akce" :multiple="true" options-url="eshop/discount?property=name" :options-url-params="{method: 'POST', body: {'_op': 'list'}}" />
-          <BaseTextBox wrap="col-lg-4" name="priority" label="Priorita" type="number" required />
+          <BaseTextArea wrap="col-lg-12" name="description" label="Popis" />
+        </div>
+
+        <div class="form-wrapper-light mt-3">
+          <div class="row">
+            <BaseMultiSelect wrap="col-lg-4" name="currency" label="Měna" options-url="eshop/currency?property=code" :options-url-params="{method: 'POST', body: {'_op': 'list'}}" />
+            <BaseMultiSelect wrap="col-lg-8" name="country" label="Země DPH" options-url="eshop/country?property=name" :options-url-params="{method: 'POST', body: {'_op': 'list'}}" />
+          </div>
+
+          <div class="row mt-2">
+            <BaseMultiSelect wrap="col-lg-8" name="discounts" label="Akce" :multiple="true" options-url="eshop/discount?property=name" :options-url-params="{method: 'POST', body: {'_op': 'list'}}" />
+            <BaseTextBox wrap="col-lg-4" name="priority" label="Priorita" type="number" required />
+          </div>
+
+          <div class="row mt-3">
+            <BaseCheckBox name="allowDiscountLevel" wrap="col-lg-6"> Povolit procentuální slevy </BaseCheckBox>
+          </div>
+          <div class="col-lg-12 mt-2">
+            <i class="fa fa-info-circle" /> Aplikuje se vždy největší z čtveřice: procentuální slevy produktu, procentuální slevy zákazníka, slevy věrnostního programu zákazníka nebo slevového kupónu.
+            <br> Pokud je povoleno, aplikuje zmíněnou procentuální slevu. Jinak aplikuje pouze slevu v rámci cen v aktivních ceníkách.
+          </div>
+
+          <div class="row mt-3">
+            <BaseCheckBox name="isActive" wrap="col-lg-6"> Aktivní </BaseCheckBox>
+          </div>
         </div>
 
         <div class="row mt-3">
-          <BaseCheckBox name="allowDiscountLevel" wrap="col-lg-6"> Povolit procentuální slevy </BaseCheckBox>
-        </div>
-        <div class="col-lg-12 mt-2">
-          <i class="fa fa-info-circle" /> Aplikuje se vždy největší z čtveřice: procentuální slevy produktu, procentuální slevy zákazníka, slevy věrnostního programu zákazníka nebo slevového kupónu.
-          <br> Pokud je povoleno, aplikuje zmíněnou procentuální slevu. Jinak aplikuje pouze slevu v rámci cen v aktivních ceníkách.
-        </div>
-
-        <div class="row mt-3">
-          <BaseCheckBox name="isActive" wrap="col-lg-6"> Aktivní </BaseCheckBox>
+          <BaseTextBox wrap="col-lg-12" name="customLabel" label="Vlastní štítek" type="text" />
+          <div class="col-lg-12 mt-2">
+            <i class="fa fa-info-circle" /> Použitý při exportu XML produktů pro Google jako "custom_label_1".
+          </div>
+          <BaseMultiSelect wrap="col-lg-12 mt-2" name="internalRibbons" label="Interní štítky" :multiple="true" options-url="eshop/internal-ribbon?property=name" :options-url-params="{method: 'POST', body: {'_op': 'list'}}" />
+          <BaseTextBox wrap="col-lg-12 mt-2" name="lastUpdateTs" label="Poslední aktualizace" type="date" />
         </div>
       </div>
 
-      <div class="row mt-3">
-        <BaseTextBox wrap="col-lg-12" name="customLabel" label="Vlastní štítek" type="text" />
-        <div class="col-lg-12 mt-2">
-          <i class="fa fa-info-circle" /> Použitý při exportu XML produktů pro Google jako "custom_label_1".
-        </div>
-        <BaseMultiSelect wrap="col-lg-12 mt-2" name="internalRibbons" label="Interní štítky" :multiple="true" options-url="eshop/internal-ribbon?property=name" :options-url-params="{method: 'POST', body: {'_op': 'list'}}" />
-        <BaseTextBox wrap="col-lg-12 mt-2" name="lastUpdateTs" label="Poslední aktualizace" type="date" />
+      <div class="col-lg-8 ps-5">
+        <h5 class="card-title">CENY</h5>
+        <ThePriceTable v-model:page="page" v-model:on-page="onPage" v-model:order="order" :filters="filters" />
       </div>
     </div>
+
 
     <div class="row mt-3">
       <div class="col-lg-6">
@@ -67,6 +75,7 @@ withDefaults(defineProps<{
 
 const form = ref(null);
 const goBack: Ref<boolean> = ref(false);
+const { filters, clearFilters, page, onPage, order } = useTableVars();
 
 const rules = {
   name: {required},
