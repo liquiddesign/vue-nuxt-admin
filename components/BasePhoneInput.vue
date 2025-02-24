2284e93fc535679a212d913c2f89e95c5389cc99
MariMens <148960213+MariMens@users.noreply.github.com>
2025-02-24 17:17:44 +0100

    tel input - fix

--- components/BasePhoneInput.vue
+++ components/BasePhoneInput.vue
@@ -3,7 +3,7 @@
     <label v-if="label !== null" :for="$attrs['id']">
       <span v-if="((form && form.lang) || lang) && locale" class="pe-1"><BaseFlag :lang="form && form.lang.value ? form.lang.value : lang" /></span>{{ label }}
     </label>
-    <vue-tel-input :dropdown-options="dropdownOptions" v-bind="$attrs" class="form-control form-control-sm" :class="{'is-invalid': !isValid}" :value="form && name ? _get(form.data.value, name) : modelValue" :disabled="form?.disabled.value || $attrs['disabled']" @input="onChange" @validate="validate" />
+    <vue-tel-input v-model="phone" :dropdown-options="dropdownOptions" :input-options="inputOptions" v-bind="$attrs" class="form-control form-control-sm" :class="{'is-invalid': !isValid}" :disabled="form?.disabled.value || $attrs['disabled']" @on-input="onChange" @validate="validate" />
 
     <template v-if="validationErrors !== undefined">
       <div v-for="(error, index) in (validationErrors)" :key="index" class="text-danger">
@@ -33,33 +33,53 @@ const props = withDefaults(defineProps<{
   locale?: boolean,
   wrap?: string,
   name: string,
+  placeholder?: string,
   modelValue?: string|number|null,
   validation?: BaseValidation
   validationErrors?: ErrorObject[]
-}>(), { label: null, lang: undefined, locale: false, wrap: undefined, name: undefined, modelValue: undefined, validation: undefined, validationErrors: undefined });
+}>(), { label: null, lang: undefined, locale: false, wrap: undefined, name: undefined, placeholder: 'Telefonní číslo', modelValue: undefined, validation: undefined, validationErrors: undefined });
 
 const isValid: Ref<boolean> = ref(false);
-const dropdownOptions = {
+
+const dropdownOptions: object = {
   showDialCode: true,
   showFlags: true,
   showSearchBox: true,
   showDialCodeInSelection: true,
 };
 
+const inputOptions: object = {
+  name: props.name,
+  placeholder: props.placeholder,
+};
+
+const obj: Ref<any> = ref({
+  formatted: '',
+  nationalNumber: '',
+  number: '',
+  country: 'CZ',
+  valid: isValid.value,
+});
+
+const phone: ComputedRef<string> = computed(() => {
+  return obj.value?.formatted ? obj.value.formatted : _get(form.data.value, props.name);
+});
+
 const { validationObject } = useFormValidation(form, props);
 
-function onChange($event: any) {
-  const value: string|number|null = $event.target.value;
+function onChange(number: string, phoneObject: any) {
+  const value: string|number|null = number;
 
   if (form && props.name) {
-    form.updateInput(props.name, value);
+    form.updateInput(props.name, phoneObject.nationalNumber);
   }
 
   $emit('update:modelValue', value);
+  obj.value = JSON.parse(JSON.stringify(phoneObject));
 }
 
-function validate(objInfo: any) {
-  isValid.value = objInfo.valid;
+function validate(phoneObject: any) {
+  isValid.value = phoneObject.valid;
 }
 
 </script>
